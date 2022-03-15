265294f2e0bce854caad0de247bc363cdf167854
karangupta10 <kargupta13@gmail.com>
2022-03-15 20:39:15 +0530

    commit 8:40pm,mar 15

---
+++ app/src/main/java/com/example/android/sevensisters/Discover.java
@@ -0,0 +1,14 @@
+package com.example.android.sevensisters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Discover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+    }
+}
